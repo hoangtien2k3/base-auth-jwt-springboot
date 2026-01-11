4ecc6d5ddf52f0560592125adc538c24e4b72830
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2026-01-11 15:59:48 +0700

    Refactor GlobalExceptionHandler by removing unused imports for cleaner code

--- src/main/java/com/jwt/auth/auth_jwt/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jwt/auth/auth_jwt/exception/GlobalExceptionHandler.java
@@ -2,13 +2,11 @@ package com.jwt.auth.auth_jwt.exception;
 
 import com.jwt.auth.auth_jwt.dto.response.ApiResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 
 import java.util.HashMap;
 import java.util.Map;
